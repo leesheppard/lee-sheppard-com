9e8512bb4de4b485b5888f00aa5ded825e09d7dd
Lee Sheppard <lee@leesheppard.com>
2023-12-02 21:51:59 +1100

    Add turbo back in

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -14,14 +14,14 @@ import { Application } from "@hotwired/stimulus"
  * Adds support for declarative shadow DOM. Requires your HTML <head> to include:
  * `<meta name="turbo-cache-control" content="no-cache" />`
  */
-// import * as TurboShadow from "turbo-shadow"
+import * as TurboShadow from "turbo-shadow"
 
 /**
  * Uncomment the line below to add transition animations when Turbo navigates.
  * Use data-turbo-transition="false" on your <main> element for pages where
  * you don't want any transition animation.
  */
-// import "./turbo_transitions.js"
+import "./turbo_transitions.js"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
